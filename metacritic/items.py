c2e9239bd2067de163bf1676538174b7f8c54533
charvirus <jbrandon413@naver.com>
2020-05-22 16:19:34 +0900

    metacritic 작업중 현재 영화 제목 이외 몇몇 데이터들이 크롤링 안됌

---
+++ metacritic/items.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MoviewerItem(scrapy.Item):
+    # define the fields for your item here like:
+
+    #MetaMV_info
+
+    #영화 제목
+    META_movieName = scrapy.Field()
+
+    # #메타평론평점
+    # META_Cscore = scrapy.Field()
+    #
+    # #메타유저평점
+    # META_Uscore = scrapy.Field()
+    #
+    # #포스터
+    # META_poster = scrapy.Field()
+    #
+    # #장르
+    # META_genre = scrapy.Field()
+    #
+    # #관람가
+    # META_rate = scrapy.Field()
+    #
+    # #개봉일
+    # META_release = scrapy.Field()
+    #
+    # #감독
+    # META_director= scrapy.Field()
+    #
+    # #주연배우
+    # META_starring= scrapy.Field()
+    #
+    # #줄거리
+    # META_synopsis = scrapy.Field()
+
+    # name = scrapy.Field()
+    pass
---
+++ metacritic/spiders/meta_crawl.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class MovieCrawlSpider(CrawlSpider):
+    name = 'meta_crawl'
+    allowed_domains = ['metacritic.com']
+    start_urls = ['https://www.metacritic.com/browse/movies/release-date/theaters/date?page=0']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'/movie/\w'),
+             callback='parse_item',
+             follow=True),
+    )
+
+    def parse_item(self, response):
+        iq= {}
+        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        #item['name'] = response.xpath('//div[@id="name"]').get()
+        #item['description'] = response.xpath('//div[@id="description"]').get()
+
+        iq['META_movieName'] = response.xpath(
+            '//*[@id="main_content"]/div[1]/div[1]/div/table/tbody/tr/td[2]/div/table/tbody/tr/td[1]/div/div/div[1]/div/h1/text()'
+        ).extract()
+        #
+        # iq['META_Cscore'] = response.xpath(
+        #     '//*[@id="nav_to_metascore"]/div[1]/div[2]/div[1]/a/div/text()'
+        # ).extract()
+        #
+        # iq['META_Uscore'] = response.xpath(
+        #     '//*[@id="nav_to_metascore"]/div[2]/div[2]/div[1]/a/div/text()'
+        # ).extract()
+        #
+        # iq['META_poster'] = response.xpath(
+        #     '//*[@id="main_content"]/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[1]/img/text()'
+        # ).extract()
+        #
+        # iq['META_genre'] = response.xpath(
+        #     '//*[@id="main_content"]/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[5]/div[2]/span[2]/span/text()'
+        # ).extract()
+        #
+        # iq['META_rate'] = response.xpath(
+        #     '//*[@id="main_content"]/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[5]/div[3]/span[2]/text()'
+        # ).extract()
+        #
+        # iq['META_release'] = response.xpath(
+        #     '//*[@id="main_content"]/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[2]/table/tbody/tr/td/span[2]/span[2]/text()'
+        # ).extract()
+        #
+        # iq['META_director'] = response.xpath(
+        #     '//*[@id="main_content"]/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[5]/div[1]/a/span/text()'
+        # ).extract()
+        #
+        # iq['META_starring'] = response.xpath(
+        #     '//*[@id="main_content"]/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[3]/span[2]/text()'
+        # ).extract()
+        #
+        # iq['META_synopsis'] = response.xpath(
+        #     '//*[@id="summary_blurb_550205"]/span[2]/text()'
+        # ).extract()
+        return iq

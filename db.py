b19c6d64c7ef415c231bed4da540383abc52ad30
charvirus <jbrandon413@naver.com>
2020-05-29 12:51:51 +0900

    #9 metacritic 대신 로튼토마토로 변경 sql 작업했음

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+import csv
+con = sqlite3.connect("sql/db.sqlite")
+con.text_factory = str
+cur = con.cursor()
+
+cur.execute("""create table IF NOT EXISTS rotten(
+                title varchar(100),
+                critic_fresh varchar(20),
+                user_fresh varchar(20)
+                )""")
+con.commit()
+
+# query_str = "insert into rotten values (:title, :critic_fresh, :user_fresh)"
+# params = {
+#     "title":"기생충",
+#     "critic_fresh":"99%",
+#     "user_fresh":"99%"
+# }
+# cur.execute(query_str,params)
+
+csv_file = open("Moviewer/rottencrawl.csv","rt", encoding='UTF8')
+csv_reader = csv.reader(csv_file)
+movie_list = list(csv_reader)
+movie_list = movie_list[1:]
+for item in movie_list:
+    item[0] = item[0].strip()
+    item[1] = item[1].strip()
+    item[2] = item[2].strip()
+
+con.commit()
+
+cur.executemany("insert into rotten values(?,?,?)",movie_list)
+
+cur.execute("select * from rotten")
+print(cur.fetchall())

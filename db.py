c1c8ac3bbe110ea684cdc432b615bf1b86e8b8b2
charvirus <jbrandon413@naver.com>
2020-06-05 12:00:56 +0900

    씨네21,로튼토마토 크롤링 값을 하나의 데이터베이스에 넣음

--- db.py
+++ db.py
@@ -17,22 +17,53 @@ cur = con.cursor()
 #     "user_fresh":"99%"
 # }
 # cur.execute(query_str,params)
+# 로튼토마토
+# rotten_csv_file = open("rottentomato/rottencrawl.csv","rt", encoding='UTF8')
+# rotten_csv_reader = csv.reader(rotten_csv_file)
+# rotten_movie_list = list(rotten_csv_reader)
+# rotten_movie_list = rotten_movie_list[1:]
+# for item in rotten_movie_list:
+#     item[0] = item[0].strip()
+#     item[1] = item[1].strip()
+#     item[2] = item[2].strip()
+#
+# print(rotten_movie_list)
+#
+# # CSV 로 읽은 데이터 테이블에 INSERT
+#
+# cur.executemany("insert into rotten values(?,?,?)",rotten_movie_list)
+#
+# cur.execute("select * from rotten")
+# print(cur.fetchall())
 
-csv_file = open("rottentomato/rottencrawl.csv","rt", encoding='UTF8')
-csv_reader = csv.reader(csv_file)
-movie_list = list(csv_reader)
-movie_list = movie_list[1:]
-for item in movie_list:
-    item[0] = item[0].strip()
+
+
+# 씨네21
+
+# cur.execute("""create table if not exists cine21(
+#                 poster varchar(255),
+#                 title varchar(100),
+#                 date varchar(100),
+#                 audience varchar(255),
+#                 genre varchar(100),
+#                 story text,
+#                 review text,
+#                 director varchar(100),
+#                 actor varchar(255),
+#                 cine varchar(100),
+#                 netizens varchar(100))""")
+
+cine_csv_file = open("db/cine.csv","rt", encoding='utf8')
+cine_csv_reader = csv.reader(cine_csv_file)
+cine = list(cine_csv_reader)
+cine = cine[1:]
+for item in cine:
     item[1] = item[1].strip()
     item[2] = item[2].strip()
+    item[3] = item[3].strip()
+    item[4] = item[4].strip()
 
-print(movie_list)
-
-# CSV 로 읽은 데이터 테이블에 INSERT
-
-cur.executemany("insert into rotten values(?,?,?)",movie_list)
-
-cur.execute("select * from rotten")
-print(cur.fetchall())
+cur.executemany("insert into cine21 values(?,?,?,?,?,?,?,?,?,?,?)", cine)
+# cur.execute("select * from rotten")
+# print(cur.fetchall())
 con.commit()

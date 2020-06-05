3500869ab0100256eca3a14f6b93b16da8fcfe6d
charvirus <jbrandon413@naver.com>
2020-06-05 10:55:08 +0900

    #10 크롤링한 영화정보가 입력안됌
    con.commit()위치를 제대로 잡음

--- db.py
+++ db.py
@@ -4,12 +4,11 @@ con = sqlite3.connect("sql/db.sqlite")
 con.text_factory = str
 cur = con.cursor()
 
-cur.execute("""create table IF NOT EXISTS rotten(
-                title varchar(100),
-                critic_fresh varchar(20),
-                user_fresh varchar(20)
-                )""")
-con.commit()
+# cur.execute("""create table IF NOT EXISTS rotten(
+#                 title varchar(100),
+#                 critic_fresh varchar(20),
+#                 user_fresh varchar(20)
+#                 )""")
 
 # query_str = "insert into rotten values (:title, :critic_fresh, :user_fresh)"
 # params = {
@@ -19,7 +18,7 @@ con.commit()
 # }
 # cur.execute(query_str,params)
 
-csv_file = open("Moviewer/rottencrawl.csv","rt", encoding='UTF8')
+csv_file = open("rottentomato/rottencrawl.csv","rt", encoding='UTF8')
 csv_reader = csv.reader(csv_file)
 movie_list = list(csv_reader)
 movie_list = movie_list[1:]
@@ -28,9 +27,12 @@ for item in movie_list:
     item[1] = item[1].strip()
     item[2] = item[2].strip()
 
-con.commit()
+print(movie_list)
 
-cur.executemany("insert into rotten values(?,?,?)",movie_list)
+# CSV 로 읽은 데이터 테이블에 INSERT
+
+cur.executemany("insert ignore into rotten values(?,?,?)",movie_list)
 
 cur.execute("select * from rotten")
 print(cur.fetchall())
+con.commit()

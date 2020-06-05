19488899ef6189df2a1dd75cccd09951d2fdaadc
charvirus <jbrandon413@naver.com>
2020-06-05 12:53:09 +0900

    #12 SQL 값 홈페이지에 불러오기 작업중
    Jsonize 함수 추가, 로튼토마토 영화 제목 검색 함수 추가

--- flask/Moviewer.py
+++ flask/Moviewer.py
@@ -23,16 +23,33 @@ def hello():
 
     return result_json
 
-@app.route("/hello/<name>")
-def hello_to(name):
-    return "Hello,{}!".format(name)
+@app.route("/rotten/title")
+
+def get_movie_by_title():
+    title = request.args.get("title")
+
+    with get_db_con() as con:
+        cur=con.cursor()
+
+        q = "select * from rotten where title like :title"
+        param = {
+            "title": "%" + title + "%"
+        }
+
+        result = cur.execute(q,param)
+
+    result_json = jsonize(result)
+
+    return result_json
 
 @app.route("/hello")
 def hello_to_get_param():
     name = request.args.get("name")
     return "Hello, {}!!".format(name)
 
-
+def jsonize(result):
+    result_json = json.dumps(list(result.fetchall()), ensure_ascii=False).encode("utf-8")
+    return result_json
 
 
 if __name__ == "__main__":

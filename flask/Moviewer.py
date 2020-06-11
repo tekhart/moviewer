02be0e66674ca2f3351c7cd7b6381a575f380c46
charvirus <jbrandon413@naver.com>
2020-06-11 22:52:26 +0900

    #14 app.route() 3개이상 쓰면 오류나는 문제 해결

--- flask/Moviewer.py
+++ flask/Moviewer.py
@@ -13,7 +13,7 @@ def get_db_con() -> sqlite3.connect: return sqlite3.connect('../sql/db.sqlite')
 
 # 로튼토마토
 @app.route("/rotten")
-def hello():
+def get_rotten_movie():
     with get_db_con() as con:
         cur = con.cursor()
 
@@ -27,7 +27,7 @@ def hello():
 
 
 @app.route("/rotten/title")
-def get_movie_by_title():
+def get_rotten_movie_by_title():
 
     with get_db_con() as con:
         cur=con.cursor()
@@ -41,7 +41,7 @@ def get_movie_by_title():
     return result_json
 
 @app.route("/rotten/critic_score")
-def hello_to_get_param():
+def get_rotten_movie_by_cscore():
     with get_db_con() as con:
         cur = con.cursor()
 
@@ -53,45 +53,18 @@ def hello_to_get_param():
 
     return result_json
 
-# @app.route("/rotten/user_score")
-# def hello_to_get_param():
-#     with get_db_con() as con:
-#         cur = con.cursor()
-#
-#         q = "select * from rotten order by 3 DESC"
-#
-#         result = cur.execute(q)
-#
-#     result_json = jsonize(result)
-#
-#     return result_json
-#
-# #씨네21
-# @app.route("/cine")
-# def get_rotten_movie():
-#     with get_db_con() as con:
-#         cur = con.cursor()
-#
-#         q = "select * from cine21"
-#         result = cur.execute(q)
-#
-#     result_json = jsonize(result)
-#
-#     return result_json
-#
-# @app.route("/cine21/title")
-# def get_movie_by_title():
-#
-#     with get_db_con() as con:
-#         cur=con.cursor()
-#
-#         q = "select * from rotten order by 2"
-#
-#         result = cur.execute(q)
-#
-#     result_json = jsonize(result)
-#
-#     return result_json
+@app.route("/rotten/user_score")
+def get_rotten_movie_by_uscore():
+    with get_db_con() as con:
+        cur = con.cursor()
+
+        q = "select * from rotten order by 3 DESC"
+
+        result = cur.execute(q)
+
+    result_json = jsonize(result)
+
+    return result_json
 
 def jsonize(result):
     result_json = json.dumps(list(result.fetchall()), ensure_ascii=False).encode("utf-8")

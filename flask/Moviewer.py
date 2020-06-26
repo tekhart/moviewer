d2dd7ed457e220b274fc23dac55c42b54c9992d9
charvirus <jbrandon413@naver.com>
2020-06-26 11:58:00 +0900

    Moviewer 미연 수정

--- flask/Moviewer.py
+++ flask/Moviewer.py
@@ -12,7 +12,6 @@ def get_db_con() -> sqlite3.connect: return sqlite3.connect('../sql/MoviewerDB.s
 
 
 # 로튼토마토
-
 @app.route("/rotten/list/<id>")
 def get_rotten_movie(id):
     id = int(id)
@@ -25,6 +24,17 @@ def get_rotten_movie(id):
 
         q = "select * from rotten where id >= " + str(id) + " order by id asc limit 20"
         print(q)
+        result = cur.execute(q)
+
+    result_json = jsonize(result)
+
+    return result_json
+@app.route("/rotten/<id>")
+def get_rotten_movie_detail(id):
+    with get_db_con() as con:
+        cur = con.cursor()
+
+        q = "select * from rotten where id = '" + str(id) + "'"
         result = cur.execute(q)
 
     result_json = jsonize(result)
@@ -43,45 +53,9 @@ def get_rotten_movie_title(title):
 
     return result_json
 
-@app.route("/rotten/title")
-def get_rotten_movie_by_title():
 
-    with get_db_con() as con:
-        cur=con.cursor()
 
-        q = "select * from rotten order by 3"
 
-        result = cur.execute(q)
-
-    result_json = jsonize(result)
-
-    return result_json
-
-@app.route("/rotten/critic_score")
-def get_rotten_movie_by_cscore():
-    with get_db_con() as con:
-        cur = con.cursor()
-
-        q = "select * from rotten order by 4 DESC"
-
-        result = cur.execute(q)
-
-    result_json = jsonize(result)
-
-    return result_json
-
-@app.route("/rotten/user_score")
-def get_rotten_movie_by_uscore():
-    with get_db_con() as con:
-        cur = con.cursor()
-
-        q = "select * from rotten order by 5 DESC"
-
-        result = cur.execute(q)
-
-    result_json = jsonize(result)
-
-    return result_json
 
 #씨네21
 @app.route("/cine21/list/<id>")
@@ -102,47 +76,6 @@ def get_cine_movie(id):
 
     return result_json
 
-@app.route("/cine21/title")
-def get_cine_movie_by_title():
-
-    with get_db_con() as con:
-        cur=con.cursor()
-
-        q = "select * from cine21 order by 3"
-
-        result = cur.execute(q)
-
-    result_json = jsonize(result)
-
-    return result_json
-
-
-@app.route("/cine21/critic_score")
-def get_cine_movie_by_cscore():
-
-    with get_db_con() as con:
-        cur=con.cursor()
-
-        q = "select * from cine21 order by 10 DESC"
-
-        result = cur.execute(q)
-
-    result_json = jsonize(result)
-
-    return result_json
-
-@app.route("/cine21/user_score")
-def get_cine_movie_by_uscore():
-    with get_db_con() as con:
-        cur = con.cursor()
-
-        q = "select * from cine21 order by 11 DESC"
-
-        result = cur.execute(q)
-
-    result_json = jsonize(result)
-
-    return result_json
 
 @app.route("/cine21/<id>")
 def get_cine_movie_detail(id):
@@ -168,17 +101,6 @@ def get_cine_movie_title(title):
 
     return result_json
 
-@app.route("/cine21/search_genre/<genre>")
-def get_cine_movie_genre(genre):
-    with get_db_con() as con:
-        cur = con.cursor()
-
-        q = "select * from cine21 where genre like '%" + str(genre) + "%'"
-        result = cur.execute(q)
-
-    result_json = jsonize(result)
-
-    return result_json
 
 def jsonize(result):
     result_json = json.dumps(list(result.fetchall()), ensure_ascii=False).encode("utf-8")
